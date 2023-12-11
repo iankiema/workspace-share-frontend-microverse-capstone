1f4d318edcc88996d71c6ec82beadc2e7eb42098
Henok Kirubel <henokhm2@gmail.com>
2023-12-11 15:51:32 +0300

    Add React Router, pages, and simple navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Reservations from './pages/Reservations';
+
 function App() {
   return (
     <div>
-      <header>
-        Workspace Share
-      </header>
+      <header>Workspace Share</header>
+      <main>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="reservations" element={<Reservations />} />
+        </Routes>
+      </main>
     </div>
   );
 }
