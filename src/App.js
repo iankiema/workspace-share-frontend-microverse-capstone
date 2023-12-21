1055623c60824ea8b1afa6eefcbf90208ae09cac
iankiema <kiemaian.ik@gmail.com>
2023-12-21 16:49:17 +0300

    Add the packages and packagelist components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,74 @@
-import { Routes, Route } from 'react-router-dom';
+// // src/App.js
+// import React, { useEffect, useState } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   Navigate,
+// } from 'react-router-dom';
+// import PackageList from './components/PackageList';
+// import PackageDetails from './components/PackageDetails';
+// // import { checkLoginStatus } from './redux/loginSlice';
 
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Reservations from './pages/Reservations';
+// function App() {
+//   // const dispatch = useDispatch();
+//   // const loginStatus = useSelector((state) => state.login_auths.loggedin);
 
-function App() {
-  return (
-    <div className="font-montserrat">
-      <header className="text-red-500 font-bold">Workspace Share</header>
-      <main>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="reservations" element={<Reservations />} />
-        </Routes>
-      </main>
-    </div>
-  );
-}
+//   // const [userData, setUserData] = useState({});
 
-export default App;
+//   // const retrieveUserData = () => {
+//   //   // Retrieve the content from localStorage
+//   //   const userDataJSON = localStorage.getItem('userData');
+
+//   //   // Parse the JSON content
+//   //   const storedUserData = JSON.parse(userDataJSON);
+//   //   console.log(storedUserData.extractedUserData);
+//   //   return storedUserData.extractedUserData || {};
+//   // };
+
+//   // useEffect(() => {
+//   //   const fetchLoginStatus = () => {
+//   //     dispatch(checkLoginStatus());
+//   //   };
+
+//   //   // Call fetchLoginStatus when the component mounts
+//   //   if (loginStatus === 'empty') {
+//   //     fetchLoginStatus();
+//   //   }
+
+//   //   if (loginStatus === 'true') {
+//   //     setUserData(retrieveUserData());
+//   //   }
+//   // }, [dispatch, loginStatus]);
+
+//   // if (loginStatus !== 'empty')
+//   {
+//     return (
+//       <div className="body">
+//         <Router>
+//           <div className="pageSection">
+//             <Routes>
+//               <Route
+//                 path="/"
+//                 exact
+//                 element={
+//                   loginStatus === 'true' ? (
+//                     <Navigate to="/home" />
+//                   ) : (
+//                     <Navigate to="/splash" />
+//                   )
+//                 }
+//               />
+//               <Route path="/packages" element={<PackageList />} />
+//               <Route path="/packages/:slug" element={<PackageDetails />} />
+
+//             </Routes>
+//           </div>
+//         </Router>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
