7b0069df2cfe0b5374a17026d85308fedaecb528
DevaWinner <winneraaniekan@gmail.com>
2023-12-21 15:37:00 +0100

    Add book reservation form to BookReservation component

--- src/components/BookReservation.js
+++ src/components/BookReservation.js
@@ -53,6 +53,71 @@ function BookReservation() {
 	};
 
 	return (
+		<div className="container-fluid">
+			<div className="row align-items-center">
+				{" "}
+				{/* Center both horizontally and vertically */}
+				<div className="col-lg-3">
+					<Navbar />
+				</div>
+				<div className="col-lg-9 d-flex justify-content-center vh-100">
+					{" "}
+					{/* Center horizontally */}
+					<div className="card shadow my-auto">
+						<div className="card-body">
+							<h2 className="card-title">Book Reservation</h2>
+							<form onSubmit={handleSubmit}>
+								<div className="mb-3">
+									<label htmlFor="location" className="form-label">
+										Location:
+										<input
+											type="text"
+											className="form-control"
+											id="location"
+											value={location}
+											onChange={(e) => setLocation(e.target.value)}
+										/>
+									</label>
+								</div>
+								<div className="mb-3">
+									<label htmlFor="date" className="form-label">
+										Date:
+										<input
+											type="date"
+											className="form-control"
+											id="date"
+											value={date}
+											onChange={(e) => setDate(e.target.value)}
+										/>
+									</label>
+								</div>
+								<div className="mb-3">
+									<label htmlFor="package" className="form-label">
+										Select Package:
+										<select
+											className="form-select"
+											id="package"
+											value={selectedPackage}
+											onChange={(e) => setSelectedPackage(e.target.value)}
+										>
+											<option value="">Select a package</option>
+											{packages.map((pack) => (
+												<option key={pack.id} value={pack.attributes.slug}>
+													{pack.attributes.name}
+												</option>
+											))}
+										</select>
+									</label>
+								</div>
+								<button type="submit" className="btn btn-primary">
+									Book Now
+								</button>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	);
 }
 
